2f1b132fc919ff66d75af0caf28e4a3de9cd7917
PBard83 <PBard83@users.noreply.github.com>
2023-03-02 17:51:32 -0500

    Added Scene1, inputting text

---
+++ SpaceGame/Assets/Scripts/Scene1Dialogue.cs
@@ -0,0 +1,172 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene1Dialogue : MonoBehaviour {
+        public int primeInt = 1;         // This integer drives game progress!
+        public Text Char1name;
+        public Text Char1speech;
+        public Text Char2name;
+        public Text Char2speech;
+       //public Text Char3name;
+       //public Text Char3speech;
+        public GameObject DialogueDisplay;
+        public GameObject ArtChar1a;
+       //public GameObject ArtChar1b;
+       //public GameObject ArtChar2;
+        public GameObject ArtBG1;
+        public GameObject Choice1a;
+        public GameObject Choice1b;
+        public GameObject NextScene1Button;
+        public GameObject NextScene2Button;
+        public GameObject nextButton;
+       //public GameHandler gameHandler;
+       //public AudioSource audioSource;
+        private bool allowSpace = true;
+
+// initial visibility settings. Any new images or buttons need to also be SetActive(false);
+void Start(){  
+        DialogueDisplay.SetActive(false);
+        ArtChar1.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        nextButton.SetActive(true);
+
+     // Find the gameHandler:
+     // gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+   }
+
+void Update(){         // use spacebar as Next button
+        if (allowSpace == true){
+                if (Input.GetKeyDown("space")){
+                       next();
+                }
+        }
+   }
+
+//Story Units! This is the main story function. Players hit [NEXT] to progress to the next primeInt:
+public void next(){
+        primeInt = primeInt + 1;
+        if (primeInt == 1){
+                // AudioSource.Play();
+        }
+        else if (primeInt == 2){
+               ArtChar1a.SetActive(true);
+                DialogueDisplay.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "GEN. ROOM";
+                Char2speech.text = "You awaken in a darkened room as your eyes struggle to adjust to the dim light filtering in through the damaged walls.";
+        }
+       else if (primeInt ==3){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "GEN. ROOM";
+                Char2speech.text = "You have no memory of how you got here, but the sight before you leaves little doubt that something terrible has happened.";
+                //gameHandler.AddPlayerStat(1);
+        }
+       else if (primeInt == 4){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "GEN. ROOM";
+                Char2speech.text = "The room is in shambles, with gaping holes in the walls and scorch marks on the floor";
+        }
+       else if (primeInt == 5){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "GEN. ROOM";
+                Char2speech.text = "You look to the source of the ship's power - the generator.";
+                //gameHandler.AddPlayerStat(1);
+        }
+       else if (primeInt == 6){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "GEN. ROOM";
+                Char2speech.text = "It's clear that it has been sabotaged, with wires and circuitry hanging haphazardly from the exposed machinery.";
+        }
+       else if (primeInt ==7){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "GEN. ROOM";
+                Char2speech.text = "But nothing looks damaged, just pulled out of place. Perhaps you could repair it?";
+				// Turn off "Next" button, turn on "Choice" buttons
+                nextButton.SetActive(false);
+                allowSpace = false;
+                Choice1a.SetActive(true); // function Choice1aFunct()
+                Choice1b.SetActive(true); // function Choice1bFunct()
+       
+        }
+
+// ENCOUNTER AFTER CHOICE #1
+       else if (primeInt == 100){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Jeda";
+                Char2speech.text = "Then you are no use to me, and must be silenced.";
+        }
+       else if (primeInt == 101){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Jeda";
+                Char2speech.text = "Come back here! Do not think you can hide from me!";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene1Button.SetActive(true);
+        }
+
+       else if (primeInt == 200){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Jeda";
+                Char2speech.text = "Do not think you can fool me, human. Where will we find him?";
+        }
+       else if (primeInt == 201){
+                Char1name.text = "YOU";
+                Char1speech.text = "Ragu hangs out in a rough part of town. I'll take you now.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene2Button.SetActive(true);
+        }
+
+      //Please do NOT delete this MOST HOLY bracket that ends the next() function:
+     }
+
+// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch-scenes)
+        public void Choice1aFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "I don't know what you're talking about!";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 99;
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+        public void Choice1bFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "Sure, anything you want... just lay off the club.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 199;
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+
+        public void SceneChange1(){
+               SceneManager.LoadScene("Scene2a");
+        }
+        public void SceneChange2(){
+                SceneManager.LoadScene("Scene2b");
+        }
+}
