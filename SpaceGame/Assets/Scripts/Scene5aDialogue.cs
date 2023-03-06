3e9bfda2b0969958d7bca538e1ddbd5d98b87dc9
PBard83 <PBard83@users.noreply.github.com>
2023-03-06 11:41:08 -0500

    Update Scene5aDialogue.cs

--- SpaceGame/Assets/Scripts/Scene5aDialogue.cs
+++ SpaceGame/Assets/Scripts/Scene5aDialogue.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 
-public class Scene4Dialogue : MonoBehaviour {
+public class Scene5aDialogue : MonoBehaviour {
         public int primeInt = 1;         // This integer drives game progress!
         public Text Char1name;
         public Text Char1speech;
@@ -59,137 +59,80 @@ public void next(){
         else if (primeInt == 2){
                ArtChar1.SetActive(true);
                 DialogueDisplay.SetActive(true);
+                Char1name.text = "YOU";
+                Char1speech.text = "I don't know about this.";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt ==3){
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "HORIZON";
-                Char2speech.text = "Warning. Fire detected. Hazardous conditions likely.";
-        }
-       else if (primeInt ==3){
-                Char1name.text = "YOU";
-                Char1speech.text = "What's causing the fire?";
-                Char2name.text = "";
-                Char2speech.text = "";
+                Char2speech.text = "There is a battery located inside the Air Lock.";
                 //gameHandler.AddPlayerStat(1);
         }
        else if (primeInt == 4){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "HORIZON";
-                Char2speech.text = "Preliminary analysis suggests weapons damage.";
+                Char1name.text = "YOU";
+                Char1speech.text = "OK, I see it. Let's get it and get out of here.";
+                Char2name.text = "";
+                Char2speech.text = "";
         }
+				//Door Slam noise here
        else if (primeInt == 5){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "HORIZON";
-                Char2speech.text = "This would be consistent with visible damage";
+                Char1name.text = "YOU";
+                Char1speech.text = "No! Open the door!";
+                Char2name.text = "";
+                Char2speech.text = "";
                 //gameHandler.AddPlayerStat(1);
         }
        else if (primeInt == 6){
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "HORIZON";
-                Char2speech.text = "to walls and floor, as well as two bodies on the floor.";
+                Char2speech.text = "Door control unavailable.";
         }
        else if (primeInt ==7){
                 Char1name.text = "YOU";
-                Char1speech.text = "Oh no, two more dead crew.";
+                Char1speech.text = "How do I open it?";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
 		       else if (primeInt ==8){
-                Char1name.text = "YOU";
-                Char1speech.text = "I don't recognize these either. Who are they?";
-                Char2name.text = "";
-                Char2speech.text = "";
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "HORIZON";
+                Char2speech.text = "The door appears to have been locked.
+				There is a member of the crew on the other side.";
         }
 		       else if (primeInt ==9){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Data library offline.
-				Restart generator to restore access.";
+                Char1name.text = "YOU";
+                Char1speech.text = "Hey! Let me out!";
+                Char2name.text = "";
                 Char2speech.text = "";
         }
+				//Alarm noise here
 		       else if (primeInt ==10){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "HORIZON";
+                Char2speech.text = "The crewmember appears to be intent on neutralizing you.";
+		}
+		       else if (primeInt ==11){
                 Char1name.text = "YOU";
-                Char1speech.text = "Yeah, I'm working on it.
-				What do these doors lead to?";
+                Char1speech.text = "What does that mean?";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
-       else if (primeInt == 11){
+		       else if (primeInt ==12){
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "Available paths are to the air lock, the med bay, and the captain's quarters";
-                Char2speech.text = "Do not play the stupid. You will take me to him.";
-                // Turn off "Next" button, turn on "Choice" buttons
-                nextButton.SetActive(false);
-                allowSpace = false;
-                Choice1a.SetActive(true); // function Choice1aFunct()
-                Choice1b.SetActive(true); // function Choice1bFunct()
-        }
-
-// ENCOUNTER AFTER CHOICE #1
-       else if (primeInt == 100){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Then you are no use to me, and must be silenced.";
-        }
-       else if (primeInt == 101){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Come back here! Do not think you can hide from me!";
-                nextButton.SetActive(false);
-                allowSpace = false;
-                NextScene1Button.SetActive(true);
-        }
-
-       else if (primeInt == 200){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Do not think you can fool me, human. Where will we find him?";
-        }
-       else if (primeInt == 201){
-                Char1name.text = "YOU";
-                Char1speech.text = "Ragu hangs out in a rough part of town. I'll take you now.";
-                Char2name.text = "";
-                Char2speech.text = "";
-                nextButton.SetActive(false);
-                allowSpace = false;
-                NextScene2Button.SetActive(true);
-        }
-     }
-
-// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch-scenes)
-        public void Choice1aFunct(){
-                Char1name.text = "YOU";
-                Char1speech.text = "I don't know what you're talking about!";
-                Char2name.text = "";
-                Char2speech.text = "";
-                primeInt = 99;
-                Choice1a.SetActive(false);
-                Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
-        }
-        public void Choice1bFunct(){
-                Char1name.text = "YOU";
-                Char1speech.text = "Sure, anything you want... just lay off the club.";
-                Char2name.text = "";
-                Char2speech.text = "";
-                primeInt = 199;
-                Choice1a.SetActive(false);
-                Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
-        }
+                Char2name.text = "HORIZON";
+                Char2speech.text = "The Air Lock is going to open.
+				You will not survive.";
+		}
 
         public void SceneChange1(){
-               SceneManager.LoadScene("Scene2a");
-        }
-        public void SceneChange2(){
-                SceneManager.LoadScene("Scene2b");
+               SceneManager.LoadScene("Scene_Lose");
+
         }
 }
