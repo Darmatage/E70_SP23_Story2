8fa9adac6277094481c82e948904ff9b659ac8d9
PBard83 <PBard83@users.noreply.github.com>
2023-03-06 11:19:01 -0500

    Updated Scene4 dialogue, created dummy scripts for Scenes 5a, 5b, 5c

--- SpaceGame/Assets/Scripts/Scene5bDialogue.cs
+++ SpaceGame/Assets/Scripts/Scene5bDialogue.cs
@@ -105,9 +105,9 @@ public void next(){
 		       else if (primeInt ==9){
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "HORIZON";
-                Char2speech.text = "Data library offline.
+                Char2name.text = "Data library offline.
 				Restart generator to restore access.";
+                Char2speech.text = "";
         }
 		       else if (primeInt ==10){
                 Char1name.text = "YOU";
@@ -119,16 +119,77 @@ public void next(){
        else if (primeInt == 11){
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "HORIZON";
-                Char2speech.text = "Available paths are to the air lock, the med bay, and the captain's quarters.";
+                Char2name.text = "Available paths are to the air lock, the med bay, and the captain's quarters";
+                Char2speech.text = "Do not play the stupid. You will take me to him.";
                 // Turn off "Next" button, turn on "Choice" buttons
                 nextButton.SetActive(false);
                 allowSpace = false;
-                NextScene1Button.SetActive(true); // function Load Scene5a()
-                NextScene2Button.SetActive(true); // function Load Scene5b()
-				NextScene3Button.SetActive(true); // function Load Scene5c() 
-				
+                Choice1a.SetActive(true); // function Choice1aFunct()
+                Choice1b.SetActive(true); // function Choice1bFunct()
         }
 
+// ENCOUNTER AFTER CHOICE #1
+       else if (primeInt == 100){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Jeda";
+                Char2speech.text = "Then you are no use to me, and must be silenced.";
+        }
+       else if (primeInt == 101){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Jeda";
+                Char2speech.text = "Come back here! Do not think you can hide from me!";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene1Button.SetActive(true);
+        }
 
+       else if (primeInt == 200){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Jeda";
+                Char2speech.text = "Do not think you can fool me, human. Where will we find him?";
+        }
+       else if (primeInt == 201){
+                Char1name.text = "YOU";
+                Char1speech.text = "Ragu hangs out in a rough part of town. I'll take you now.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene2Button.SetActive(true);
+        }
+     }
+
+// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch-scenes)
+        public void Choice1aFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "I don't know what you're talking about!";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 99;
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+        public void Choice1bFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "Sure, anything you want... just lay off the club.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 199;
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+
+        public void SceneChange1(){
+               SceneManager.LoadScene("Scene2a");
+        }
+        public void SceneChange2(){
+                SceneManager.LoadScene("Scene2b");
+        }
 }
