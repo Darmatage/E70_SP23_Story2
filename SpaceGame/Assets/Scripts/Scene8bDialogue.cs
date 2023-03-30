831d8cc3f0f49fc752137971ff85ff26ae03d11b
GMax009 <gratis.maxwell@gmail.com>
2023-03-30 09:53:08 -0400

    armory and barracks scripts finished

--- SpaceGame/Assets/Scripts/Scene8bDialogue.cs
+++ SpaceGame/Assets/Scripts/Scene8bDialogue.cs
@@ -18,10 +18,9 @@ public class Scene8bDialogue : MonoBehaviour {
        //public GameObject ArtChar1b;
        //public GameObject ArtChar2;
         public GameObject ArtBG1;
-        public GameObject Choice1a;
-        public GameObject Choice1b;
+        public GameObject Choice1a;                                 // search foot locker
+        public GameObject Choice1b;                                 // search hanging uniforms)
         public GameObject NextScene1Button;
-        public GameObject NextScene2Button;
         public GameObject nextButton;
        //public GameHandler gameHandler;
        //public AudioSource audioSource;
@@ -35,7 +34,6 @@ void Start(){
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
         NextScene1Button.SetActive(false);
-        NextScene2Button.SetActive(false);
         nextButton.SetActive(true);
 
      // Find the gameHandler:
@@ -59,8 +57,8 @@ public void next(){
         else if (primeInt == 2){
                ArtChar1.SetActive(true);
                 DialogueDisplay.SetActive(true);
-                Char1name.text = "";
-                Char1speech.text = "";
+                Char1name.text = "YOU";
+                Char1speech.text = "This room is in rough shape.";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
@@ -71,8 +69,8 @@ public void next(){
                 Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
+				Char3name.text = "H.O.R.I.Z.O.N.";
+                Char3speech.text = "It appears that crew members left quickly.";
                 //gameHandler.AddPlayerStat(1);
         }
        else if (primeInt == 4){
@@ -80,12 +78,12 @@ public void next(){
                 Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
+				Char3name.text = "H.O.R.I.Z.O.N.";
+                Char3speech.text = "Assessment: Remnants of emergency evacuation.";
         }
        else if (primeInt == 5){
-                Char1name.text = "";
-                Char1speech.text = "";
+                Char1name.text = "YOU";
+                Char1speech.text = "What can we find here?";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
@@ -97,95 +95,159 @@ public void next(){
                 Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
+				Char3name.text = "H.O.R.I.Z.O.N.";
+                Char3speech.text = "Assessing... \n There is a foot locker you may be able to open.";
         }
        else if (primeInt ==7){
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt == 8){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-                // Turn off "Next" button, turn on "Choice" buttons
+				Char3name.text = "H.O.R.I.Z.O.N.";
+                Char3speech.text = "There is a rack of hanging uniforms that you can search. It is possible that you may find useful items or supplies.";
+				// Turn off "Next" button, turn on "Choice" buttons
                 nextButton.SetActive(false);
                 allowSpace = false;
                 Choice1a.SetActive(true); // function Choice1aFunct()
                 Choice1b.SetActive(true); // function Choice1bFunct()
         }
 
-// ENCOUNTER AFTER CHOICE #1
-       else if (primeInt == 100){
-                Char1name.text = "";
-                Char1speech.text = "";
+																				//ENCOUNTER AFTER CHOICE 1a: foot locker
+       else if (primeInt == 31){
+                Char1name.text = "YOU";
+                Char1speech.text = "Let's see what we've got...";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
                 Char3speech.text = "";
         }
-       else if (primeInt == 101){
-                Char1name.text = "";
-                Char1speech.text = "";
+		else if (primeInt == 32){
+                Char1name.text = "YOU";
+                Char1speech.text = "Some playing cards, a few magazines, some basic toiletries...";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
                 Char3speech.text = "";
-                nextButton.SetActive(false);
+        }
+		else if (primeInt == 33){
+                Char1name.text = "YOU";
+                Char1speech.text = "... and a small box.";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		else if (primeInt == 34){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "H.O.R.I.Z.O.N.";
+                Char3speech.text = "Directive: Search box.";
+        }
+		else if (primeInt == 35){
+                Char1name.text = "YOU";
+                Char1speech.text = "Thanks for the tip...";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		else if (primeInt == 36){
+                Char1name.text = "YOU";
+                Char1speech.text = "Looks like a toolbox. Poorly maintained.";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		else if (primeInt == 37){
+                Char1name.text = "YOU";
+                Char1speech.text = "All that's left is this screw driver. Might come in handy though.";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		else if (primeInt == 38){
+                Char1name.text = "YOU";
+                Char1speech.text = "Doesn't seem like there's much else in here. Time to move on.";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+				nextButton.SetActive(false);
                 allowSpace = false;
-                NextScene1Button.SetActive(true);
+				NextScene1Button.SetActive(true);             // Back to Hallway
         }
-
-       else if (primeInt == 200){
-                Char1name.text = "";
-                Char1speech.text = "";
+																	// After choice 1b: search uniforms
+		else if (primeInt == 71){
+                Char1name.text = "YOU";
+                Char1speech.text = "Nothing in the pockets. Just a bunch of uniforms";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
                 Char3speech.text = "";
         }
-       else if (primeInt == 201){
-                Char1name.text = "";
-                Char1speech.text = "";
+		else if (primeInt == 72){
+                Char1name.text = "YOU";
+                Char1speech.text = "I guess I'll see what's in the box.";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
                 Char3speech.text = "";
-                nextButton.SetActive(false);
+        }
+		else if (primeInt == 73){
+                Char1name.text = "YOU";
+                Char1speech.text = "Looks like a toolbox. Poorly maintained.";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		else if (primeInt == 74){
+                Char1name.text = "YOU";
+                Char1speech.text = "All that's left is this screw driver. Might come in handy though.";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		else if (primeInt == 75){
+                Char1name.text = "YOU";
+                Char1speech.text = "Doesn't seem like there's much else in here. Time to move on.";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+				nextButton.SetActive(false);
                 allowSpace = false;
-                NextScene2Button.SetActive(true);
+                NextScene1Button.SetActive(true);     // back to hallway
         }
      }
 
 // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch-scenes)
-        public void Choice1aFunct(){
-                Char1name.text = "";
-                Char1speech.text = "";
+        public void Choice1aFunct(){                                                  //search foot locker
+                Char1name.text = "YOU";
+                Char1speech.text = "I'll check the locker first.";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
                 Char3speech.text = "";
-                primeInt = 99;
+                primeInt = 30;
                 Choice1a.SetActive(false);
                 Choice1b.SetActive(false);
                 nextButton.SetActive(true);
                 allowSpace = true;
         }
-        public void Choice1bFunct(){
-                Char1name.text = "";
-                Char1speech.text = "";
+        public void Choice1bFunct(){                                             // search uniforms
+                Char1name.text = "YOU";
+                Char1speech.text = "I'll check the uniforms first.";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
                 Char3speech.text = "";
-                primeInt = 199;
+                primeInt = 70;
                 Choice1a.SetActive(false);
                 Choice1b.SetActive(false);
                 nextButton.SetActive(true);
@@ -193,9 +255,7 @@ public void next(){
         }
 
         public void SceneChange1(){
-               SceneManager.LoadScene("Scene2a");
-        }
-        public void SceneChange2(){
-                SceneManager.LoadScene("Scene2b");
+				 GameHandler.hasScrewdriver = true;
+                 SceneManager.LoadScene("Scene7");
         }
 }
