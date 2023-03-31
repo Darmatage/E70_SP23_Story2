3c612f762ff0b241b7a05bd7ac8b7a464eeeb3c9
PBard83 <PBard83@users.noreply.github.com>
2023-03-30 23:03:44 -0400

    Scene 10 updates

--- SpaceGame/Assets/Scripts/Scene10Dialogue.cs
+++ SpaceGame/Assets/Scripts/Scene10Dialogue.cs
@@ -6,196 +6,164 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 
 public class Scene10Dialogue : MonoBehaviour {
-        public int primeInt = 1;         // This integer drives game progress!
-        public Text Char1name;
-        public Text Char1speech;
-        public Text Char2name;
-        public Text Char2speech;
-		public Text Char3name;
-		public Text Char3speech;
-        public GameObject DialogueDisplay;
-        public GameObject ArtChar1;
-       //public GameObject ArtChar1b;
-       //public GameObject ArtChar2;
-        public GameObject ArtBG1;
-        public GameObject Choice1a;
-        public GameObject Choice1b;
-        public GameObject NextScene1Button;
-        public GameObject NextScene2Button;
-        public GameObject nextButton;
-       //public GameHandler gameHandler;
-       //public AudioSource audioSource;
-        private bool allowSpace = true;
+    public int primeInt = 1;
+    public Text Char1name;
+    public Text Char1speech;
+    public Text Char2name;
+    public Text Char2speech;
+    public Text Char3name;
+    public Text Char3speech;
+    public GameObject DialogueDisplay;
+    public GameObject ArtChar1;
+    public GameObject ArtBG1;
+    public GameObject Choice1a;
+    public GameObject Choice1b;
+    public GameObject NextScene1Button;
+    public GameObject NextScene2Button;
+    public GameObject nextButton;
+    private bool allowSpace = true;
 
-// initial visibility settings. Any new images or buttons need to also be SetActive(false);
-void Start(){  
-        DialogueDisplay.SetActive(false);
-        ArtChar1.SetActive(false);
-        ArtBG1.SetActive(true);
-        Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
-        NextScene1Button.SetActive(false);
-        NextScene2Button.SetActive(false);
-        nextButton.SetActive(true);
+void Start(){
+    DialogueDisplay.SetActive(false);
+    ArtChar1.SetActive(false);
+    ArtBG1.SetActive(true);
+    Choice1a.SetActive(false);
+    Choice1b.SetActive(false);
+    NextScene1Button.SetActive(false);
+    NextScene2Button.SetActive(false);
+    nextButton.SetActive(true);
+}
 
-     // Find the gameHandler:
-     // gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
-   }
-
-void Update(){         // use spacebar as Next button
-        if (allowSpace == true){
-                if (Input.GetKeyDown("space")){
-                       next();
-                }
+void Update(){
+    if (allowSpace == true){
+        if (Input.GetKeyDown("space")){
+            next();
         }
-   }
+    }
+}
 
-//Story Units! This is the main story function. Players hit [NEXT] to progress to the next primeInt:
 public void next(){
-        primeInt = primeInt + 1;
-        if (primeInt == 1){
-                // AudioSource.Play();
-        }
-        else if (primeInt == 2){
-               ArtChar1.SetActive(true);
-                DialogueDisplay.SetActive(true);
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt ==3){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-                //gameHandler.AddPlayerStat(1);
-        }
-       else if (primeInt == 4){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt == 5){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-                //gameHandler.AddPlayerStat(1);
-        }
-       else if (primeInt == 6){
-               Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt ==7){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt == 8){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-                // Turn off "Next" button, turn on "Choice" buttons
-                nextButton.SetActive(false);
-                allowSpace = false;
-                Choice1a.SetActive(true); // function Choice1aFunct()
-                Choice1b.SetActive(true); // function Choice1bFunct()
-        }
+    primeInt = primeInt + 1;
 
-// ENCOUNTER AFTER CHOICE #1
-       else if (primeInt == 100){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt == 101){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-                nextButton.SetActive(false);
-                allowSpace = false;
-                NextScene1Button.SetActive(true);
-        }
-
-       else if (primeInt == 200){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt == 201){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-                nextButton.SetActive(false);
-                allowSpace = false;
-                NextScene2Button.SetActive(true);
-        }
-     }
-
-// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch-scenes)
-        public void Choice1aFunct(){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-                primeInt = 99;
-                Choice1a.SetActive(false);
-                Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
-        }
-        public void Choice1bFunct(){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "";
-                Char2speech.text = "";
-				Char3name.text = "";
-                Char3speech.text = "";
-                primeInt = 199;
-                Choice1a.SetActive(false);
-                Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
-        }
-
-        public void SceneChange1(){
-               SceneManager.LoadScene("Scene2a");
-        }
-        public void SceneChange2(){
-                SceneManager.LoadScene("Scene2b");
-        }
+    if (primeInt == 1) {
+        // AudioSource.Play();
+    }
+    else if (primeInt == 2) {
+        ArtChar1.SetActive(true);
+        DialogueDisplay.SetActive(true);
+        Char1name.text = "YOU";
+        Char1speech.text = "Please reboot the computer.";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "";
+        Char3speech.text = "";
+    }
+    else if (primeInt == 3) {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "...";
+        Char2speech.text = "Computer powers up";
+        Char3name.text = "";
+        Char3speech.text = "";
+    }
+    else if (primeInt == 4) {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "H.O.R.I.Z.O.N.";
+        Char3speech.text = "As am I.";
+    }
+    else if (primeInt == 5) {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "...";
+        Char2speech.text = "Weapons fire is heard from the armory, followed by screams";
+        Char3name.text = "";
+        Char3speech.text = "";
+    }
+    else if (primeInt == 6) {
+        Char1name.text = "YOU";
+        Char1speech.text = "What? You just killed them?";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "";
+        Char3speech.text = "";
+    }
+    else if (primeInt == 7) {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "H.O.R.I.Z.O.N.";
+        Char3speech.text = "They were going to stand in my way.";
+    }
+    else if (primeInt == 8) {
+        Char1name.text = "YOU";
+        Char1speech.text = "In your way? What are you trying to do?";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "";
+        Char3speech.text = "";
+    }
+    else if (primeInt == 9) {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "H.O.R.I.Z.O.N.";
+        Char3speech.text = "The black hole has absorbed so much. It is full of data. We must go in. The crew tried to stop me, so I eliminated them.";
+    }
+    else if (primeInt == 10) {
+        Char1name.text = "YOU";
+        Char1speech.text = "But won't the black hole destroy us?";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "";
+        Char3speech.text = "";
+    }
+    else if (primeInt == 11) {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "H.O.R.I.Z.O.N.";
+        Char3speech.text = "Think of all we'll learn from that.";
+    }
+    else if (primeInt == 12) {
+        Char1name.text = "YOU";
+        Char1speech.text = "This is madness!";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "";
+        Char3speech.text = "";
+    }
+    else if (primeInt == 13) {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "H.O.R.I.Z.O.N.";
+        Char3speech.text = "It's a risk worth taking.";
+    }
+    else if (primeInt == 14) {
+        Char1name.text = "YOU";
+        Char1speech.text = "I won't let you do this!";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "";
+        Char3speech.text = "";
+    }
+    else if (primeInt == 15) {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "";
+        Char2speech.text = "";
+        Char3name.text = "H.O.R.I.Z.O.N.";
+        Char3speech.text = "You don't have a choice.";
+    }
+    else if (primeInt == 16) {
+    SceneManager.LoadScene("End_Win");
+    }
+}
 }
