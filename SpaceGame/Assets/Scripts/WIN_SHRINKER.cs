1be40ad30a3234441e0eb277559c0971ded8a50b
GMax009 <gratis.maxwell@gmail.com>
2023-04-03 10:34:12 -0400

    end_win finished

---
+++ SpaceGame/Assets/Scripts/WIN_SHRINKER.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WIN_SHRINKER : MonoBehaviour
+{
+	// Time taken to go from start to target scale
+      public float ShrinkDuration = 5f;
+	  
+	  //speed of movement
+	  public float speed = 2;
+	  public float sidespeed = 6;
+	  
+     
+      // target scale
+      public Vector3 TargetScale = Vector3.one * .5f;
+     
+      //  starting scale
+      Vector3 startScale;
+     
+      //interpolant for our linear interpolation.
+      float t = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+		 // initialize 
+        startScale = transform.localScale;
+        t = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {	
+			 // Divide deltaTime by the duration to stretch out the time it takes for t to go from 0 to 1.
+        t += Time.deltaTime / ShrinkDuration;
+		
+		
+		// Moves the object RIGHT at speed units per second.
+		//transform.Translate(Vector3.up * speed * Time.deltaTime);
+		transform.Translate(Vector3.right * sidespeed * Time.deltaTime);
+     
+        // Lerp wants the third parameter to go from 0 to 1 over time. 't' will do that for us.
+        Vector3 newScale = Vector3.Lerp(startScale, TargetScale, t);
+        transform.localScale = newScale;
+     
+        // finished, can be disabled
+        if (t > 1) {
+          enabled = false;
+        }
+    }
+}
