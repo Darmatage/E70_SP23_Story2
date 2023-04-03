7360c57ab9333dfef48fee0ee738ede13837c56b
GMax009 <gratis.maxwell@gmail.com>
2023-04-02 22:58:02 -0400

    updated scene 9

--- SpaceGame/Assets/Scripts/Scene9Dialogue.cs
+++ SpaceGame/Assets/Scripts/Scene9Dialogue.cs
@@ -19,6 +19,7 @@ public class Scene9Dialogue : MonoBehaviour {
 	public GameObject ArtChar3;
 	public GameObject ArtChar4;
     public GameObject ArtBG1;
+	public GameObject ArtBG2;
     public GameObject NextScene1Button;
     public GameObject nextButton;
     //public GameHandler gameHandler;
@@ -33,6 +34,7 @@ public class Scene9Dialogue : MonoBehaviour {
 		ArtChar3.SetActive(false);
 		ArtChar4.SetActive(false);
         ArtBG1.SetActive(true);
+		ArtBG2.SetActive(false);
         NextScene1Button.SetActive(false);
         nextButton.SetActive(true);
 
@@ -56,74 +58,155 @@ public class Scene9Dialogue : MonoBehaviour {
         }
         else if (primeInt == 2){
             DialogueDisplay.SetActive(true);
-			ArtChar1.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "...";
-            Char2speech.text = "You return to the generator room, the boxy machine looming before you.";
+            Char2speech.text = "You hurry through the ship, following H.O.R.I.Z.O.N.’s directions for the quickest path back.";
             Char3name.text = "";
             Char3speech.text = "";
         }
         else if (primeInt ==3){
-            Char1name.text = "";
+            ArtBG2.SetActive(true);
+			ArtBG1.SetActive(false);
+			Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "";
-            Char2speech.text = "";
-            Char3name.text = "H.O.R.I.Z.O.N.";
-            Char3speech.text = "Directive: Insert battery array.";
-            //gameHandler.AddPlayerStat(1);
+            Char2name.text = "...";
+            Char2speech.text = "Before you know it, you find yourself back in the generator room. It’s just as you left it.";
+            Char3name.text = "";
+            Char3speech.text = "";
         }
         else if (primeInt == 4){
-            ArtChar1.SetActive(false);
-			ArtChar2.SetActive(true);
 			Char1name.text = "YOU";
-            Char1speech.text = "OK, here goes nothing.";
+            Char1speech.text = "Finally. This will all be over soon, just need to get this array slotted into the generator.";
             Char2name.text = "";
             Char2speech.text = "";
             Char3name.text = "";
             Char3speech.text = "";
         }
         else if (primeInt == 5){
-            ArtChar2.SetActive(false);
-			ArtChar3.SetActive(true);
+			ArtChar1.SetActive(true);
 			Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "...";
-            Char2speech.text = "The generator powers on.";
+            Char2speech.text = "You approach the hulking machine.";
             Char3name.text = "";
             Char3speech.text = "";
-            //gameHandler.AddPlayerStat(1);
         }
         else if (primeInt == 6){
+			ArtChar1.SetActive(false);
+			ArtChar2.SetActive(true);
+			Char1name.text = "";
+            Char1speech.text = "";
+			Char2name.text = "...";
+			Char2speech.text = "Unslinging the battery array, you heft it toward the empty ports. The batteries slot in with a solid click.";
+			Char3name.text = "";
+			Char3speech.text = "";
+		}
+        else if (primeInt == 7){
+			Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "...";
+            Char2speech.text = "The error message is replaced with new text.";
+            Char3name.text = "";
+            Char3speech.text = "";
+        }
+		else if (primeInt == 8){
             Char1name.text = "YOU";
-            Char1speech.text = "It worked!";
+            Char1speech.text = "“Power cells detected. Press Start.”";
 			Char2name.text = "";
 			Char2speech.text = "";
 			Char3name.text = "";
 			Char3speech.text = "";
-}
-        else if (primeInt == 7){
-            ArtChar3.SetActive(false);
-			ArtChar4.SetActive(true);
+		}
+		else if (primeInt == 9){
+            Char1name.text = "YOU";
+            Char1speech.text = "Simple enough.";
+			Char2name.text = "";
+			Char2speech.text = "";
+			Char3name.text = "";
+			Char3speech.text = "";
+		}
+		else if (primeInt == 10){
+            ArtChar2.SetActive(false);
+			ArtChar3.SetActive(true);
 			Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "";
-            Char2speech.text = "";
-            Char3name.text = "H.O.R.I.Z.O.N.";
-            Char3speech.text = "With the power restored, the Bridge is now accessible.";
-        }
-        else if (primeInt == 8){
+			Char2name.text = "...";
+			Char2speech.text = "The machine immediately hums to life, electricity crackling across it for a moment as diagnostic text surges across its display.";
+			Char3name.text = "";
+			Char3speech.text = "";
+		}
+		else if (primeInt == 11){
+			Char1name.text = "";
+            Char1speech.text = "";
+			Char2name.text = "...";
+			Char2speech.text = "In the distance, you can hear the clank and whine of systems coming back online throughout the ship.";
+			Char3name.text = "";
+			Char3speech.text = "";
+		}
+		else if (primeInt == 12){
+			ArtChar3.SetActive(false);
+			ArtChar4.SetActive(true);
+            Char1name.text = "YOU";
+            Char1speech.text = "Finally…";
+			Char2name.text = "";
+			Char2speech.text = "";
+			Char3name.text = "";
+			Char3speech.text = "";
+		}
+		else if (primeInt == 13){
             Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "";
-            Char2speech.text = "";
-            Char3name.text = "H.O.R.I.Z.O.N.";
-            Char3speech.text = "Please proceed to the Bridge to reboot the Prime Intelligence.";
-            // Turn off "Next" button, turn on "Choice" buttons
-            nextButton.SetActive(false);
+			Char2name.text = "";
+			Char2speech.text = "";
+			Char3name.text = "H.O.R.I.Z.O.N.";
+			Char3speech.text = "Main power restored. Directive: Proceed to Bridge.";
+		}
+		else if (primeInt == 14){
+            Char1name.text = "YOU";
+            Char1speech.text = "You think it’s open now?";
+			Char2name.text = "";
+			Char2speech.text = "";
+			Char3name.text = "";
+			Char3speech.text = "";
+		}
+		else if (primeInt == 15){
+            Char1name.text = "";
+            Char1speech.text = "";
+			Char2name.text = "";
+			Char2speech.text = "";
+			Char3name.text = "H.O.R.I.Z.O.N.";
+			Char3speech.text = "Affirmative. Furthermore, the Primary Intelligence must be re-awakened, to return the Orpheus to its course.";
+		}
+		else if (primeInt == 16){
+            ArtChar4.SetActive(false);
+			Char1name.text = "YOU";
+            Char1speech.text = "So we’re going home?";
+			Char2name.text = "";
+			Char2speech.text = "";
+			Char3name.text = "";
+			Char3speech.text = "";
+		}
+		else if (primeInt == 17){
+            Char1name.text = "";
+            Char1speech.text = "";
+			Char2name.text = "";
+			Char2speech.text = "";
+			Char3name.text = "H.O.R.I.Z.O.N.";
+			Char3speech.text = "Affirmative.";
+		}
+		else if (primeInt == 18){
+			Char1name.text = "YOU";
+            Char1speech.text = "Thank goodness… ";
+			Char2name.text = "";
+			Char2speech.text = "";
+			Char3name.text = "";
+			Char3speech.text = "";
+			nextButton.SetActive(false);
             allowSpace = false;
 			NextScene1Button.SetActive(true);
-        }
+		}
+   
 
     } // Closing brace for the next() function
 
